372240d46be0650d769d93ad791c3f745a183e1f
petkokpetkov <ppetko@softserveinc.com>
2021-12-25 12:10:25 +0200

    removed console log

--- homework3/scripts/click.list.item.js
+++ homework3/scripts/click.list.item.js
@@ -2,7 +2,6 @@ document.getElementById("meals-list").addEventListener("click", (e) => {
   fetch("https://api.npoint.io/51ed846bdd74ff693d7e/meals")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.find((x) => x.id === e.target.id));
       const clickedItemFromList = data.find((item) => item.id === e.target.id);
 
       const dialog = document.getElementById("additional-info-dialog");
